011d9ba1f83251407f3d020f932e90807448c3ea
Marky <85974485+M4Rk9@users.noreply.github.com>
2026-07-20 12:34:40 +0530

    Update next.config.ts for image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   images: {
-    formats: ["image/avif", "image/webp"]
-  }
+    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
